5e1995a55490b92ce2631fda7fd6599c7813ab68
balazs4 <balazs4web@gmail.com>
2017-05-22 23:10:40 +0200

    :tada: mediaklikk live links on-demand

---
+++ index.js
@@ -0,0 +1,41 @@
+const log = require('debug')('mediaklikk');
+const phantom = require('phantom');
+
+const getLink = async src => {
+  log('Creating phantom instance');
+  const instance = await phantom.create([
+    '--ignore-ssl-errors=yes',
+    '--load-images=no'
+  ]);
+
+  log('Creating page object');
+  const page = await instance.createPage();
+
+  log(`Opening ${src}`);
+  await page.open(src);
+
+  log('Extracting content');
+  const content = await page.property('content');
+
+  const [rawLink] = content.match(/(http:.*\.m3u8\?v=5)/g);
+  const link = rawLink.replace(/\\\//g, '/');
+  log(`Found link ${link}`);
+
+  log('Closing phantom instance');
+  await instance.exit();
+  return link;
+};
+
+module.exports = async (req, res) => {
+  const streamId = req.url.split('/')[1];
+  if (streamId === 'favicon.ico') return null;
+  log(`Looking for '${streamId}'`);
+  const src = `http://player.mediaklikk.hu/player/player-inside-full3.php?userid=mtva&streamid=${streamId}&noflash=yes`;
+  const url = await getLink(src);
+  log(`Result ${streamId} => ${url}`);
+
+  res.writeHead(302, {
+    Location: url
+  });
+  res.end();
+};
